a5cc9498f15ba96472d222fe853e8360dbcddc05
Jannis Wingeyer <jannis@wingeyer.com>
2024-10-28 14:29:06 +0100

    feat: Add blog api and validate response plus added logging interceptor

---
+++ src/app/components/blog-overview/blog-overview.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BlogEntry, BlogEntrySchema } from '../../types/blogEntry';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { BlogApiResponse, BlogApiResponseSchema } from '../../types/blogApiResponse';
+import { CommonModule } from '@angular/common';
+
+const ValidationSchema = BlogApiResponseSchema(BlogEntrySchema);
+
+@Component({
+  selector: 'blog-overview',
+  templateUrl: './blog-overview.component.html',
+  styleUrls: ['./blog-overview.component.scss'],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    CommonModule
+  ]
+})
+export class BlogOverviewComponent implements OnInit {
+
+  blogs: BlogEntry[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loadBlogs();
+  }
+
+  loadBlogs(): void {
+    this.http.get<BlogApiResponse<BlogEntry>>('https://d-cap-blog-backend---v2.whitepond-b96fee4b.westeurope.azurecontainerapps.io/entries')
+      .subscribe(data => {
+        const result = ValidationSchema.safeParse(data);
+
+        if (!result.success) {
+          console.error(result.error);
+
+          return;
+        }
+
+        this.blogs = result.data.data;
+      });
+  }
+}
+
---
+++ src/app/interceptors/loggingInterceptor.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Outgoing request to: ', req.url);
+    return next.handle(req).pipe(
+      tap(event => {
+        console.log('Response received from: ', req.url);
+      })
+    );
+  }
+}
+
